23eff2d5295fd4caaec8dfe471732a1659293f6f
Nick <nvanalfen2@gmail.com>
2020-10-17 14:14:11 -0400

    Add some clue functions to LogicPuzzle class

--- LogicPuzzle.py
+++ LogicPuzzle.py
@@ -9,10 +9,10 @@ import pandas as pd
 import numpy as np
 
 # CLUE TYPES
-# Type 1:       Ai is Bj
-# Type 2:       Ai is not Bj
-# Type 3:       Ai is Bj or Ck (xor implied)
-# Type 4:       each of [list of Ai],
+# Type 1:       Ai is Bj                            # SOLVED - set_element
+# Type 2:       Ai is not Bj                        # SOLVED - set_not_element
+# Type 3:       Ai is Bj or Ck (xor implied)        # SOLVED - is_one_of
+# Type 4:       each of [list of Ai],               # IN PROGRESS
 #               is one of [list of A'j]
 # Type 5a:      AiB > AjB
 # Type 5b:      AiB = AiB + x
@@ -35,7 +35,7 @@ class LogicPuzzle:
     def read_categories(self, f_name):
         df = pd.read_csv(f_name)
         for label in df.columns:
-            self.categories[label] = np.array( df[label] )
+            self.categories[label] = list( df[label] )
         self.generate_sets()
     
     # Generates sets for the given categories
@@ -76,3 +76,87 @@ class LogicPuzzle:
     # Unpacks key into cat_A, el_A, cat_B
     def unpack_key(self, key):
         return key[0], key[1], key[2]
+    
+    # Get the combined lengths of all of the sets
+    def get_total_length(self):
+        size = 0
+        for key in self.sets:
+            size += len( self.sets[key] )
+        return size
+    
+    # Puzzle will be solved when all sets have length 1
+    def is_solved(self):
+        solved = True
+        for key in self.sets:
+            solved = solved and len(self.sets[key]) == 1
+        return solved
+    
+    # returns the complement of the universal set with set_A
+    def complement(self, A_set, universal):
+        return universal-A_set
+
+    # Intersects 
+    def is_element(self, A_set, element):
+        return A_set & set([element])
+    
+    def exclude_element(self, A_set, element):
+        if element in A_set:
+            A_set.remove(element)
+        return A_set
+    
+    # CLUE FUNCTIONS - HANDLES THE CLUES THEMSELVES ###########################
+    
+    # Type 1 clue solution
+    def set_element(self, el_A, el_B, inner_call=False):
+        category_A = self.get_parent_set_category(el_A)
+        category_B = self.get_parent_set_category(el_B)
+        all_els_A = self.get_full_parent_set(el_A)           # get all elements of parent set of el_A
+        set_A = self.sets[ (category_A, el_A, category_B) ]
+        set_A = self.is_element(set_A, el_B)
+        self.sets[ (category_A, el_A, category_B) ] = set_A
+        
+        for el in all_els_A:
+            if el != el_A:
+                temp_set = self.sets[ (category_A, el, category_B) ]
+                temp_set = self.exclude_element(temp_set, el_B)
+                self.sets[ (category_A, el, category_B) ] = temp_set
+        
+        # Because sets are symmetric, if el_A on cat_B is el_B, then el_B on cat_A is el_A
+        if not inner_call:
+            self.set_element(el_B, el_A, inner_call=True)
+        
+        return True             # Always return True, this clue has no extra steps
+    
+    # Type 2 clue solution
+    def set_not_element(self, el_A, el_B, inner_call=False):
+        category_A = self.get_parent_set_category(el_A)
+        category_B = self.get_parent_set_category(el_B)
+        set_A = self.sets[ (category_A, el_A, category_B) ]
+        set_A = self.exclude_element(set_A, el_B)
+        self.sets[ (category_A, el_A, category_B) ] = set_A
+        
+        if not inner_call:
+            self.set_not_element(el_B, el_A, inner_call=True)
+    
+        return True             # Always return True, this clue has no extra steps
+    
+    # Type 3 clue solution
+    # For clue that says el_A is either el_B or el_C where el_A, el_B, and el_C are
+    # members of sets A, B, and C respectively
+    def is_one_of(self, el_A, el_B, el_C):
+        category_A = self.get_parent_set_category(el_A)
+        category_B = self.get_parent_set_category(el_B)
+        category_C = self.get_parent_set_category(el_C)
+        set_AB = self.sets[ (category_A, el_A, category_B) ]
+        set_AC = self.sets[ (category_A, el_A, category_C) ]
+        
+        self.set_not_element(el_B, el_C)
+        
+        if len( set_AB & set( [el_B] ) ) == 0:
+            return self.set_element(el_A, el_C)
+        elif len( set_AC & set( [el_C] ) ) == 0:
+            return self.set_element(el_A, el_B)
+            
+        return False
+    
+    # OTHER LOGIC FUNCTIONS - IN ADDITION TO THE CLUES, USE LOGIC TO DRAW CONCLUSIONS
