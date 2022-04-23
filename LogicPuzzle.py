b5d7b29aed9ebb6fb4bb8d2167855bab467757ba
Nick <nvanalfen2@gmail.com>
2022-04-23 13:09:58 -0400

    Separate function types

--- LogicPuzzle.py
+++ LogicPuzzle.py
@@ -1,17 +1,21 @@
 import numpy as np
 import pandas as pd
 
-class LogcPuzzle:
+class LogicPuzzle:
     def __init__(self, category_f_name=None, rule_f_name=None):
         self.categories = []                # list of category titles, first title will be the key category
         self.category_values = {}           # dict from category name to set of values in that category
-        self.full_sets = {}                 # dict from (category_A_name, category_A_value) to set of remaining category_B_values
+        self.full_sets = {}                 # dict from (category_A_name, category_A_value, category_B_name) to set of remaining category_B_values
 
         if not category_f_name is None:
             self.read_categories(category_f_name)
         if not rule_f_name is None:
             self.read_rules(rule_f_name)
+        
+        self.create_sets()
     
+    ##### Setup Functions ##############################################################################################################
+
     def read_categories(self, f_name):
         try:
             f = open(f_name)
@@ -26,3 +30,30 @@ class LogcPuzzle:
 
     def read_rules(self, f_name):
         pass
+
+    def create_sets(self):
+        for i in range(len(self.categories)):
+            cat_A = self.categories[i]
+            for j in range(len(self.categories))[i+1:]:
+                cat_B = self.categories[j]
+                for val in self.category_values[ cat_A ]:
+                    self.full_sets[ ( cat_A, val, cat_B ) ] = self.category_values[ cat_B ]
+                for val in self.category_values[ cat_B ]:
+                    self.full_sets[ ( cat_B, val, cat_A ) ] = self.category_values[ cat_A ]
+    
+    ##### Auxilliary Functions #########################################################################################################
+
+    def peek(a):
+        val = a.pop()
+        a.add(val)
+        return val
+    
+    ##### Rule Functions ###############################################################################################################
+
+    def a_is_b(self, cat_A, el_A, cat_B, el_B, inner=False):
+        self.full_sets[ (cat_A, el_A, cat_B) ] = set([el_B])
+        for val in self.category_values[cat_A]:
+            if val != cat_A:
+                pass
+
+    ##### General Logic Functions ######################################################################################################
